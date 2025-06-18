0316697860c3d62c30ba193d43072055356f79a6
zryaapsa <pandu.surya0412@gmail.com>
2025-06-18 18:50:46 +0700

    animated-navbar

--- lib/frontend/pages/animated_narbar.dart
+++ lib/frontend/pages/animated_narbar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_ourecycle/frontend/pages/home_screen.dart';
+import 'package:test_ourecycle/frontend/pages/setting_screen.dart';
+import 'package:test_ourecycle/frontend/pages/transaksi_screen.dart';
+
 
 class AnimationBar extends StatefulWidget {
   const AnimationBar({super.key});
@@ -8,8 +12,34 @@ class AnimationBar extends StatefulWidget {
 }
 
 class _AnimationBarState extends State<AnimationBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    TransaksiScreen(),
+    SettingScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text('Home Screen')]));
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Color(0xFF00AA13),
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w500),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Transaksi',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+      ),
+    );
   }
 }
